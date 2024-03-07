fd9dac85a1d4267e98865c03d13b5caa9abd15f4
Hampus Bosson <hampus.bosson@outlook.com>
2024-03-07 13:26:04 +0100

    Write tests for ship factory function and also write code that passes the tests in code file

--- src/code/ship.js
+++ src/code/ship.js
@@ -1,9 +1,30 @@
 const Ship = (length) => {
+  if (length > 4 || length < 1) {
+    throw new Error('Invalid ship length: Length must be between 1 and 4.');
+  };
+
   const ship = {
     length: length,
     hits: 0,
     sunk: false,
   };
 
+  const getLength = () => {
+    return ship.length; 
+  };
+
+  const hit = () => {
+    ship.hits++; 
+    if (ship.hits === ship.length) 
+      ship.sunk = true; 
+  };
   
+  const isSunk = () => {
+    return ship.sunk; 
+  };
+
+
+  return { getLength, hit, isSunk };
 };
+
+module.exports = Ship; 
