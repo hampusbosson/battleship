aee954741f5e1cb51bd5b0a67d7b676845657f96
Hampus Bosson <hampus.bosson@outlook.com>
2024-03-25 10:43:26 +0100

    Add test and code for player module

---
+++ src/code/player.js
@@ -0,0 +1,23 @@
+const Player = (gameboard) => {
+
+    const attack = (enemyBoard, x, y) => {
+        enemyBoard.receiveAttack(x,y); 
+        computerAttack(gameboard);
+    };
+
+    const computerAttack = (playerBoard) => {
+        const randomX = Math.floor(Math.random() * 10);
+        const randomY = Math.floor(Math.random() * 10); 
+        if (playerBoard.squareAttacked(randomX, randomY)) {
+            computerAttack(playerBoard); 
+        } else {
+            playerBoard.receiveAttack(randomX, randomY);
+        }
+    };
+
+    return {
+        attack
+    }; 
+};
+
+module.exports = Player; 
