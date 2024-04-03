84f302a28f35c48268f91ee5e646615800953c18
Hampus Bosson <hampus.bosson@outlook.com>
2024-04-03 18:49:44 +0200

    Incorporate placeShipIcon function in placeShip function

--- src/dom/placeShips.js
+++ src/dom/placeShips.js
@@ -1,6 +1,7 @@
 import helper from './helper';
 import Gameboard from '../code/gameboard';
 import Ship from '../code/ship';
+import CombatSetup from './combatSetup';
 
 const PlaceShips = (() => {
   let currentMouseOverHandlers = new Map();
@@ -30,11 +31,8 @@ const PlaceShips = (() => {
     return false; // Return false if no selected ship is found
   };
 
-  const getAxis = () => {
-    
-  };
-
   const shipBoxSelector = () => {
+    let shipLength = 0;
     const shipBox = document.querySelectorAll('.ship-box');
     shipBox.forEach((ship) => {
       ship.addEventListener('click', () => {
@@ -50,8 +48,9 @@ const PlaceShips = (() => {
         ship.lastChild.classList.add('ship-text-highlight');
 
         let shipId = ship.id;
-        let shipLength = helper.ships[shipId].length;
-        lightGridSquares(shipLength);
+        shipLength = helper.ships[shipId].length;
+        lightGridSquares(shipLength, CombatSetup.getActiveAxis());
+        
       });
     });
 
@@ -62,7 +61,10 @@ const PlaceShips = (() => {
         resetSelectedShip();
       }
     });
+
+    return shipLength;
   };
+  
 
   const resetShipBoxes = () => {
     const shipBox = document.querySelectorAll('.ship-box');
@@ -85,48 +87,48 @@ const PlaceShips = (() => {
     });
   };
 
-  const lightGridSquares = (shipLength) => {
+  const lightGridSquares = (shipLength, axis) => {
     const gridSquares = document.querySelectorAll('.grid-square');
-    const gridWidth = 10;
-
+    const gridWidth = 10; // Assuming a grid width of 10 for this example
+  
     gridSquares.forEach((element, index) => {
-      // First, remove existing event listeners if they exist
+      // Remove existing event listeners if they exist
       if (currentMouseOverHandlers.has(element)) {
-        element.removeEventListener(
-          'mouseover',
-          currentMouseOverHandlers.get(element),
-        );
-        element.removeEventListener(
-          'mouseout',
-          currentMouseOutHandlers.get(element),
-        );
+        element.removeEventListener('mouseover', currentMouseOverHandlers.get(element));
+        element.removeEventListener('mouseout', currentMouseOutHandlers.get(element));
       }
-
+  
       // Define new handlers
       const mouseOverHandler = () => {
-        let endIndex = index + shipLength - 1;
-        let currentRow = Math.floor(index / gridWidth);
-        let endRow = Math.floor(endIndex / gridWidth);
-
-        if (currentRow === endRow) {
-          for (let i = index; i <= endIndex && i < index + gridWidth; i++) {
-            if (i < gridSquares.length) {
-              gridSquares[i].classList.add('grid-highlight');
-            }
+        // Calculate endIndex differently based on the axis
+        let endIndex;
+        if (axis === 'x') {
+          endIndex = index + shipLength - 1;
+        } else if (axis === 'y') {
+          endIndex = index + (shipLength - 1) * gridWidth;
+        }
+  
+        for (let i = index; i <= endIndex; i += (axis === 'x' ? 1 : gridWidth)) {
+          if (axis === 'x' && Math.floor(i / gridWidth) !== Math.floor(index / gridWidth)) {
+            // If highlighting horizontally and we've moved to a new row, stop.
+            break;
+          }
+          if (i < gridSquares.length) {
+            gridSquares[i].classList.add('grid-highlight');
           }
         }
       };
-
+  
       const mouseOutHandler = () => {
-        gridSquares.forEach((square) => {
+        gridSquares.forEach(square => {
           square.classList.remove('grid-highlight');
         });
       };
-
+  
       // Update the map with new handlers
       currentMouseOverHandlers.set(element, mouseOverHandler);
       currentMouseOutHandlers.set(element, mouseOutHandler);
-
+  
       // Attach the new event listeners
       element.addEventListener('mouseover', mouseOverHandler);
       element.addEventListener('mouseout', mouseOutHandler);
@@ -140,24 +142,35 @@ const PlaceShips = (() => {
         if (!isShipSelected()) {
             return;
         }
-
+        //remove highlight on gridsquares
+        helper.resetGridSquares();
+        //find selected ship
         let selectedShipId = null;
         for (let id in selectedShip) {
           if (selectedShip[id] === true) {
             selectedShipId = parseInt(id);
           }
         }
-
+        //ship data 
+        let rotationAxis = CombatSetup.getActiveAxis();
         let xAxis = parseInt(square.id.charAt(0), 10);
         let yAxis = parseInt(square.id.charAt(1), 10);
         let shipLength = helper.ships[selectedShipId].length;
-
+        let shipName = helper.ships[selectedShipId].name;
+        //rotate ship if needed
         let ship = Ship(shipLength, selectedShipId);
+        if (rotationAxis === 'y') {
+            ship.rotateShip();
+        }
+        
         playerBoard.placeShip(ship, xAxis, yAxis);
-        console.log(playerBoard.getBoard());
+        console.log(playerBoard.getBoard()); 
+
+        helper.placeShipIcon(square, shipName, rotationAxis, shipLength);
 
         const selectedShipBox = document.getElementById(`${selectedShipId}`);
         selectedShipBox.classList.add('ship-box-placed');
+        selectedShipBox.lastChild.classList.add('ship-text-placed');
 
         resetSelectedShip();
       });
