4ee49fa04e30ce33f01abecb2f8ec9032f5f86e8
Hampus Bosson <hampus.bosson@outlook.com>
2024-04-15 14:20:45 +0200

    Add combat module in dom folder, used to display the combat between two players

---
+++ src/dom/combat.js
@@ -0,0 +1,44 @@
+import helper from './helper';
+
+const Combat = (() => {
+  const loadCombatContent = () => {
+    helper.restorePage();
+    const content = document.getElementById('content');
+
+    content.append(loadBattleCard());
+  };
+
+  const loadBattleCard = () => {
+    const battleSection = helper.create('section', {
+      className: 'battle-card',
+    });
+
+    battleSection.append(loadPlayerSide(), loadComputerSide());
+
+    return battleSection;
+  };
+
+  const loadPlayerSide = () => {
+    const playerSide = helper.create('section', { className: 'player-side' });
+    const playerHeader = helper.create('div', { className: 'player-header', textContent: 'FRIENDLY WATERS'} );
+    const playerGrid = helper.loadGridSquare();
+    playerSide.classList.add('player-side');
+
+    playerSide.append(playerHeader, playerGrid);
+    return playerSide;
+  };
+
+  const loadComputerSide = () => {
+    const computerSide = helper.create('section', { className: 'computer-side' });
+    const computerHeader = helper.create('div',  {className: 'computer-header', textContent: 'ENEMY WATERS' });
+    const computerGrid = helper.loadGridSquare();
+    computerSide.classList.add('computer-side');
+
+    computerSide.append(computerHeader, computerGrid);
+    return computerSide;
+  };
+
+  return { loadCombatContent };
+})();
+
+export default Combat;
